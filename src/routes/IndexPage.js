b2f6c500cf49c2b1c19df06a5ee317608bd14924
18166049498 <18166049498@kxhtml.com>
2018-03-29 14:03:47 +0800

    test

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'dva';
 import { Button } from 'antd';
 import AppHeader from '../components/AppHeader';
-import AppMenu from '../components/AppMenu';
 import styles from './IndexPage.css';
 
 function IndexPage({ children }) {
