d1a9ddb7f9c582aa109e842f6b245cae88404a33
18166049498 <18166049498@kxhtml.com>
2018-03-29 13:44:59 +0800

    modify

--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -1,58 +1,8 @@
 import React from 'react';
 import { Menu, Icon } from 'antd';
 import { Link } from 'dva/router';
+import { connect } from 'dva';
 const SubMenu = Menu.SubMenu;
-
-const menuData = [
-  {
-    title: '票圈管理1',
-    children: [{
-      path: 'extend/1',
-      title: 'test',
-      key: '1dsg322'
-    }]
-  },
-  {
-    title: '活动管理',
-    children: [{
-      path: 'extend/2',
-      title: 'test',
-      key: '13dasfafg22'
-    }]
-  },
-  {
-    title: '优惠管理',
-    children: [{
-      path: 'extend/3',
-      title: 'test',
-      key: 'fasdsfaf'
-    }]
-  },
-  {
-    title: '投放管理',
-    children: [{
-      path: 'extend/4',
-      title: 'test',
-      key: '13fsdafa22'
-    }]
-  },
-  {
-    title: '消息中心',
-    children: [{
-      path: 'extend/5',
-      title: 'test',
-      key: '1322dsafsdfa'
-    }]
-  },
-  {
-    title: '意见反馈',
-    children: [{
-      path: 'extend/feedback',
-      title: '意见反馈',
-      key: '13222334sdakfjl'
-    }]
-  }
-]
 
 class AppMenu extends React.Component {
   // submenu keys of first level
@@ -79,7 +29,7 @@ class AppMenu extends React.Component {
         style={{ width: 200, height: '100%', boxShadow: "2px 0 4px 0 rgba(0,0,0,0.15)", float: 'left' }}
       >
         {
-          menuData.map((item, index) => {
+          this.props.menuData.map((item, index) => {
             return (
               <SubMenu key={index} title={item.title}>
                 {
@@ -100,4 +50,11 @@ class AppMenu extends React.Component {
   }
 };
 
-export default AppMenu;
+export default connect(
+  state => {
+    const { menuData } = state.app;
+    return {
+      menuData
+    };
+  }
+)(AppMenu);
